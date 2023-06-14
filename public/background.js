5d83fa830a42692572fb56ae330b8bc92b96bbef
fancohen <62612357+fancohen@users.noreply.github.com>
2023-06-14 23:50:17 +0800

    Update background.js

--- public/background.js
+++ public/background.js
@@ -1,3 +1,3 @@
 const body = document.querySelector('body');
 const imageUrl = 'https://www.linweifan.com/sail.jpg';
-body.style.backgroundImage = `url(${imageUrl})`;
+body.style.cssText = `background-image: url(${imageUrl}) !important;`
