61e6e275ecff97f3fba07e241fb7e7d8a585706d
fancohen <62612357+fancohen@users.noreply.github.com>
2024-10-21 00:53:08 +0200

    Update background.js

--- public/background.js
+++ public/background.js
@@ -1,4 +1,4 @@
-const imageUrl = 'https://www.linweifan.com/sail.jpg';
+const imageUrl = 'https://qn-n4-lz02.obs.cn-north-4.myhuaweicloud.com/meiyan-common-img/photo/6715899a68eee8m1koz07n6918.jpg';
 const styleElement = document.createElement('style');
 styleElement.innerHTML = `
   body {
